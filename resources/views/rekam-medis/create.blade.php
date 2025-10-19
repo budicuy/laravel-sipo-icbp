88743e790f333e9f623eb41fff2ef665d17bfe61
budicuy <personal.budicuy@gmail.com>
2025-10-19 23:43:00 +0800

    feat(rekam-medis): ganti label keterangan menjadi anamnesa dan tambah default waktu periksa

--- resources/views/rekam-medis/create.blade.php
+++ resources/views/rekam-medis/create.blade.php
@@ -280,7 +280,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
                             </div>
-                            <input type="time" id="waktu_periksa" name="waktu_periksa" value="{{ old('waktu_periksa') }}" class="w-full pl-10 pr-4 py-2.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500">
+                            <input type="time" id="waktu_periksa" name="waktu_periksa" value="{{ old('waktu_periksa', date('H:i')) }}" class="w-full pl-10 pr-4 py-2.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500">
                         </div>
                     </div>
 
@@ -386,7 +386,7 @@
                             <!-- Keterangan -->
                             <div class="md:col-span-2">
                                 <label class="block text-sm font-semibold text-gray-700 mb-2">
-                                    Keterangan / Catatan
+                                    Anamnes
                                 </label>
                                 <textarea name="keluhan[0][keterangan]" rows="3" class="w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500" placeholder="Masukkan catatan medis, anjuran dokter, atau informasi penting lainnya..."></textarea>
                             </div>
